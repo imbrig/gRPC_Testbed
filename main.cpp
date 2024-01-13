52996813a91f084277d11c4a868a125f8a6f7555
Brigido Rodriguez <brigido.rodriquez@image-metrics.com>
2024-01-12 22:05:38 -0800

    Better proto python and adding streaming request.

--- main.cpp
+++ main.cpp
@@ -5,29 +5,41 @@
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
+using grpc::ServerWriter;
 using grpc::Status;
 using testbed::HelloRequest;
-using testbed::HelloReply;
-using testbed::Greeter;
+using testbed::HelloResponse;
+using testbed::HelloStreamResponse;
+using testbed::HelloService;
 
-class GreeterServiceImpl final : public Greeter::Service {
-  Status SayHello(ServerContext* context, const HelloRequest* request,
-                  HelloReply* reply) override {
+class HelloServiceImpl final : public HelloService::Service {
+  Status Hello(ServerContext* context, const HelloRequest* request, HelloResponse* reply) override {
     std::cout << "Got a request from the client." << std::endl;
     std::string prefix("Hello silly face, ");
-    reply->set_message(prefix + request->name());
+    reply->add_values(prefix + request->value());
+    return Status::OK;
+  }
+
+  Status HelloStream(ServerContext* context, const HelloRequest* request, ServerWriter<HelloStreamResponse>* writer) override {
+    std::cout << "Got a streaming request from the client." << std::endl;
+    for (uint32_t i = 0; i < request->extra_times(); ++i) {
+      HelloStreamResponse response;
+      response.set_value("Hello " + request->value() + " number " + std::to_string(i));
+      writer->Write(response);
+    }
     return Status::OK;
   }
 };
 
 void RunServer() {
   std::string server_address("127.0.0.1:50051");
-  GreeterServiceImpl service;
+  HelloServiceImpl service;
 
   ServerBuilder builder;
   builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
   builder.RegisterService(&service);
   std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
   server->Wait();
 }
 
--- testbed_client.py
+++ testbed_client.py
@@ -1,13 +1,22 @@
-from __future__ import print_function
-import grpc
-import testbed_pb2
-import testbed_pb2_grpc
+import asyncio
+import build.proto_py.testbed as testbed
 
-def run():
-    channel = grpc.insecure_channel('127.0.0.1:50051')
-    stub = testbed_pb2_grpc.GreeterStub(channel)
-    response = stub.SayHello(testbed_pb2.HelloRequest(name='client'))
-    print("Greeter client received: " + response.message)
+from grpclib.client import Channel
+
+async def main():
+    channel = Channel(host="127.0.0.1", port=50051)
+    service = testbed.HelloServiceStub(channel)
+    response = await service.hello(testbed.HelloRequest(value="hello", extra_times=1))
+
+    for value in response.values:
+        print("Greeter client received: " + value)
+
+    async for response in service.hello_stream(testbed.HelloRequest(value="hello", extra_times=1)):
+        print(response)
+
+    # don't forget to close the channel when done!
+    channel.close()
 
 if __name__ == '__main__':
-    run()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
